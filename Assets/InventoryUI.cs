f7dc390f03e7fe528f40165d57d91daccb016b58
Stofas991 <stofaj@seznamm.cz>
2023-09-28 19:14:53 +0200

    Picking up items for one character finished

---
+++ Assets/InventoryUI.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryUI : MonoBehaviour
+{
+    public Transform itemsParent;
+
+    Inventory inventory;
+
+    InventorySlot[] slots;
+    // Start is called before the first frame update
+    void Start()
+    {
+        inventory = Inventory.Instance;
+        inventory.OnItemChangedCallBack += UpdateUI;
+
+        slots = itemsParent.GetComponentsInChildren<InventorySlot>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void UpdateUI()
+    {
+        for (int i = 0; i < slots.Length; i++)
+        {
+            if (i < inventory.items.Count)
+            {
+                slots[i].AddItem(inventory.items[i]);
+            }
+            else
+            {
+                slots[i].ClearSlot();
+            }
+        }
+        
+    }
+}
---
+++ Assets/UnitStats.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitStats : MonoBehaviour
+{
+    public int MaxHealth = 100;
+    public int attackDamage = 0;
+    public float attackSpeed = 2f;
+    public float attackCooldown = 0f;
+    int CurrentHealth;
+
+    public HealthBarScript HealthBar;
+    // Start is called before the first frame update
+    void Start()
+    {
+        CurrentHealth = MaxHealth;
+        HealthBar.SetMaxHealt(MaxHealth);
+
+    }
+
+    public void TakeDamage(int Damage)
+    {
+        CurrentHealth -= Damage;
+
+        HealthBar.SetHealth(CurrentHealth);
+        // Play hurt animation
+
+        if (CurrentHealth <= 0)
+        {
+            Die();
+        }
+
+    }
+
+    void Die() 
+    {
+        //die animation
+
+        //disable enemy
+        Destroy(gameObject);
+        Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
+    }
+}
